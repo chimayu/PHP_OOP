04bd95060382276b9c9b7c06afc9187a18fa3232
mayumi <mayumi@asadigital.net>
2019-08-01 11:51:38 +0800

    DB接続

--- create.php
+++ create.php
@@ -1,8 +1,29 @@
 <?php
 
+require_once('Models/Todo.php');
+
 // ADDボタンを押したら、
 // ユーザーが入力した内容をDBに保存して
 // 一覧画面に戻る
+
+// ユーザーが入力した内容を取得
+// $taskという変数にユーザーが入力した内容を代入
+$task = $_POST['task'];
+
+// echo '<pre>';
+// var_dump($task);
+// exit;
+
+// DBに保存
+$todo = new Todo();
+
+// Todoクラスのインスタンス
+// createメソッドを実行
+$todo->create($task);
+
+// echo '<pre>';
+// var_dump($todo);
+// exit;
 
 // 一覧画面に戻る
 header('location: index.php');
